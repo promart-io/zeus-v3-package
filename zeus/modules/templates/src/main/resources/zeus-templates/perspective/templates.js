ddea2e27882704369fcb9790bc3c6f9dda13b2f2
Pavlov <i302281@c02q81pxg8wl.dhcp.sofl.sap.corp>
2018-05-02 17:52:04 +0300

    Content added

---
+++ zeus/modules/templates/src/main/resources/zeus-templates/perspective/templates.js
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2018 SAP and others.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * Contributors:
+ * SAP - initial API and implementation
+ */
+
+exports.getPerspective = function() {
+	return {
+		"name": "Templates",
+		"link": "../zeus-templates/index.html",
+		"order": "2",
+		"image": "th"
+	};
+};
